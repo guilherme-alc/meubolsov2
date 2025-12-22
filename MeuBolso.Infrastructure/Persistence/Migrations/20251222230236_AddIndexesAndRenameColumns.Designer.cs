8af60c6d25483aff27b8a35f8f0315ebd5683b17
guilherme-alc <guilhermealc01@gmail.com>
2025-12-22 20:03:41 -0300

    Adiciona migrations para os ajutes de nome das colunas e adição de índices

--- MeuBolso.Infrastructure/Persistence/Migrations/20251222230236_AddIndexesAndRenameColumns.Designer.cs
+++ MeuBolso.Infrastructure/Persistence/Migrations/20251222230236_AddIndexesAndRenameColumns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MeuBolso.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MeuBolso.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(MeuBolsoDbContext))]
-    partial class MeuBolsoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251222230236_AddIndexesAndRenameColumns")]
+    partial class AddIndexesAndRenameColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
